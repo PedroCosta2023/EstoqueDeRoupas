0ad5dc25dd92178084b621c25857e504761c6853
PedroCosta2023 <lucaspedrolucio@gmail.com>
2023-10-17 19:24:07 -0300

    Fist commit

---
+++ src/aplicativo/Clientes.java
@@ -0,0 +1,11 @@
+package aplicativo;
+
+public class Clientes {
+    private String cnpj;
+    private String cpf;
+    private String telefone;
+    private String nome;
+    private String email;
+    private String endereco;
+
+}
---
+++ src/aplicativo/Vendedores.java
@@ -0,0 +1,11 @@
+package aplicativo;
+
+public class Vendedores {
+    private String nomeCompleto;
+    private String email;
+    private String cpf;
+    private String rg;
+    private String endereco;
+
+
+}
